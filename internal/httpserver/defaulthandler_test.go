d578079bfda4a8db9fd812245e2af36c144d09b4
infraweavers <infraweavers@codeweavers.net>
2021-02-10 21:44:36 +0000

    Test http response code as well as http body

--- internal/httpserver/defaulthandler_test.go
+++ internal/httpserver/defaulthandler_test.go
@@ -3,22 +3,32 @@ package httpserver
 import (
 	"net/http"
     "net/http/httptest"
+    "io/ioutil"
     "testing"
 )
 
 func TestDefaultHandler(t *testing.T) {
-    t.Run("returns API root endpoints", func(t *testing.T) {
+    t.Run("with valid credentials, returns API root endpoints", func(t *testing.T) {
         request, _ := http.NewRequest(http.MethodGet, "/", nil)
         responseWriter := httptest.NewRecorder()
         request.SetBasicAuth("test", "secret")
 
         DefaultHandler(responseWriter, request)
+        response := responseWriter.Result()
 
-        expected := `{"endpoints": "/v1/"}`
-        output := responseWriter.Body.String()
-
-        if expected != output {
-            t.Error("Test Failed: Expected: {}, Got: {}", expected, output)
+        expectedStatusCode := 200
+        outputStatusCode = response.StatusCode
+        
+        if expectedStatusCode != outputStatusCode {
+            t.Error("Test Failed: Expected: {}, Got: {}", expectedStatusCode, outputStatusCode)
+        }
+        
+        expectedBody := `{"endpoints": "/v1/"}`
+        outputBodyBytes, _ := ioutil.ReadAll(response.Body)
+        outputBody := string(outputBodyBytes)
+        
+        if expectedBody != outputBody {
+            t.Error("Test Failed: Expected: {}, Got: {}", expectedBody, outputBody)
         }
     })
 }
