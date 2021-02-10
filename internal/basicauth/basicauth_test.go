260622b0963c3bfd0c8781a498aeb42df1b8045a
infraweavers <infraweavers@codeweavers.net>
2021-02-10 19:52:27 +0000

    replace single test with an array of tests

--- internal/basicauth/basicauth_test.go
+++ internal/basicauth/basicauth_test.go
@@ -1,27 +1,43 @@
 package basicauth
 
 import (
+	"net/http"
+	"net/http/httptest"
     "testing"
 )
 
 
-func TestIsKnownCredential(t *testing.T) {
-    var tests = []struct {
-        username    string
-        password    string
-        expected    bool
-    }{
-        {"test",    "secret",       true},
-        {"user",    "secret",       false},
-        {"test",    "password",     false},
-        {"user",    "password",     false},
-        {"test",    "",             false},
-        {"",        "password",     false},
-        {"",        "",             false},
-    }
+var testCredentials = []struct {
+    username    string
+    password    string
+    expected    bool
+}{
+    {"test",    "secret",       true},
+    {"user",    "secret",       false},
+    {"test",    "password",     false},
+    {"user",    "password",     false},
+    {"test",    "",             false},
+    {"",        "password",     false},
+    {"",        "",             false},
+}
 
-    for _, test := range tests {
+func TestIsKnownCredential(t *testing.T) {
+    for _, test := range testCredentials {
         if output := IsKnownCredential(test.username, test.password); output != test.expected {
+            t.Error("Test Failed: Input: {}:{}, Expected: {}, Got: {}", test.username, test.expected, output)
+        }
+    }
+}
+
+func TestIsAuthorised(t *testing.T) {
+
+	request, _ := http.NewRequest("GET", "/", nil)
+    wwriter := http.ResponseWriter
+
+    for _, test := range testCredentials {
+        req.SetBasicAuth(username, password)
+
+        if output := IsAuthorised(writer, request)); output != test.expected {
             t.Error("Test Failed: Input: {}:{}, Expected: {}, Got: {}", test.username, test.expected, output)
         }
     }
