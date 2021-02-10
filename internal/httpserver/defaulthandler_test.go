ab8e4dead63a5d76b652c6315862fb5b5a771233
infraweavers <infraweavers@codeweavers.net>
2021-02-10 21:49:17 +0000

    Test the appropriate responses are returned for 'no credentials' and 'invalid credentials'

--- internal/httpserver/defaulthandler_test.go
+++ internal/httpserver/defaulthandler_test.go
@@ -10,14 +10,14 @@ import (
 func TestDefaultHandler(t *testing.T) {
     t.Run("with valid credentials, returns API root endpoints", func(t *testing.T) {
         request, _ := http.NewRequest(http.MethodGet, "/", nil)
-        responseWriter := httptest.NewRecorder()
         request.SetBasicAuth("test", "secret")
+        responseWriter := httptest.NewRecorder()
 
         DefaultHandler(responseWriter, request)
         response := responseWriter.Result()
 
         expectedStatusCode := 200
-        outputStatusCode = response.StatusCode
+        outputStatusCode := response.StatusCode
         
         if expectedStatusCode != outputStatusCode {
             t.Error("Test Failed: Expected: {}, Got: {}", expectedStatusCode, outputStatusCode)
@@ -29,6 +29,37 @@ func TestDefaultHandler(t *testing.T) {
         
         if expectedBody != outputBody {
             t.Error("Test Failed: Expected: {}, Got: {}", expectedBody, outputBody)
+        }
+    })
+    
+    t.Run("with no credentials, returns 401 unauthorized", func(t *testing.T) {
+        request, _ := http.NewRequest(http.MethodGet, "/", nil)
+        responseWriter := httptest.NewRecorder()
+        
+        DefaultHandler(responseWriter, request)
+        response := responseWriter.Result()
+
+        expectedStatusCode := 401
+        outputStatusCode := response.StatusCode
+        
+        if expectedStatusCode != outputStatusCode {
+            t.Error("Test Failed: Expected: {}, Got: {}", expectedStatusCode, outputStatusCode)
+        }
+    })
+    
+    t.Run("with invalid credentials, returns 403 forbidden", func(t *testing.T) {
+        request, _ := http.NewRequest(http.MethodGet, "/", nil)
+        request.SetBasicAuth("foo", "bar")
+        responseWriter := httptest.NewRecorder()
+        
+        DefaultHandler(responseWriter, request)
+        response := responseWriter.Result()
+
+        expectedStatusCode := 403
+        outputStatusCode := response.StatusCode
+        
+        if expectedStatusCode != outputStatusCode {
+            t.Error("Test Failed: Expected: {}, Got: {}", expectedStatusCode, outputStatusCode)
         }
     })
 }
