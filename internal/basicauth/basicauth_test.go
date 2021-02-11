cb3d8749778b43b6598800a8a2c1e095c5dfc4ab
infraweavers <infraweavers@codeweavers.net>
2021-02-11 18:42:54 +0000

    consistency with defaulthandler tests

--- internal/basicauth/basicauth_test.go
+++ internal/basicauth/basicauth_test.go
@@ -6,42 +6,48 @@ import (
     "testing"
 )
 
+type credential struct {
+    username string
+    password string
+}
+
+func tester(t *testing.T, cred credential) bool {
+    request, _ := http.NewRequest(http.MethodGet, "/", nil)
+    responseWriter := httptest.NewRecorder()
+    request.SetBasicAuth("cred.username", "cred.password")
+
+    return IsAuthorised(responseWriter, request)
+}
+
 func TestIsAuthorised(t *testing.T) {
-    t.Run("returns appropriate responses with valid/invalid credentials", func(t *testing.T) {
-        var testCredentials = []struct {
-            username    string
-            password    string
-            expected    bool
-        }{
-            {"test",    "secret",       true},
-            {"user",    "secret",       false},
-            {"test",    "password",     false},
-            {"user",    "password",     false},
-            {"test",    "",             false},
-            {"",        "password",     false},
-            {"",        "",             false},
-        }
-        
-        request, _ := http.NewRequest("GET", "/", nil)
-        responseWriter := httptest.NewRecorder()
+    var testCases = []struct {
+        testCredential credential
+        expected       bool
+    }{
+        {credential{"test", "secret"}, true},
+        {credential{"user", "secret"}, false},
+        {credential{"test", "password"}, false},
+        {credential{"user", "password"}, false},
+        {credential{"test", ""}, false},
+        {credential{"", "password"}, false},
+        {credential{"", ""}, false},
+    }
 
-        for _, test := range testCredentials {
-            request.SetBasicAuth(test.username, test.password)
-            output := IsAuthorised(responseWriter, request)
+    for _, testCase := range testCases {
+        t.Run("returns appropriate responses with valid/invalid credentials", func(t *testing.T) {
+            output := tester(t, testCase.testCredential)
 
-            if output != test.expected {
-                t.Error("Test Failed: Input: {}:{}, Expected: {}, Got: {}", test.username, test.expected, output)
+            if output != testCase.expected {
+                t.Error("Test Failed: Input: {}, Expected: {}, Got: {}", testCase.testCredential, testCase.expected, output)
             }
-        }
-    })
-    
+        })
+    }
+
     t.Run("returns false when no credentials are supplied", func(t *testing.T) {
-        request, _ := http.NewRequest("GET", "/", nil)
+        request, _ := http.NewRequest(http.MethodGet, "/", nil)
         responseWriter := httptest.NewRecorder()
-        
-        expected := false
         output := IsAuthorised(responseWriter, request)
-        
+
         if output != expected {
             t.Error("Test Failed: Expected: {}, Got: {}", expected, output)
         }
