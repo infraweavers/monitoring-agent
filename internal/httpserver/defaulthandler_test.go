9706e575d9f9d8bea6d4f40bac2dc67e132d774a
infraweavers <infraweavers@codeweavers.net>
2021-02-11 15:40:01 +0000

    VS Code formatting

--- internal/httpserver/defaulthandler_test.go
+++ internal/httpserver/defaulthandler_test.go
@@ -1,9 +1,9 @@
 package httpserver
 
 import (
-	"net/http"
-    "net/http/httptest"
     "io/ioutil"
+    "net/http"
+    "net/http/httptest"
     "testing"
 )
 
@@ -18,46 +18,46 @@ func TestDefaultHandler(t *testing.T) {
 
         expectedStatusCode := 200
         outputStatusCode := response.StatusCode
-        
+
         if expectedStatusCode != outputStatusCode {
             t.Error("Test Failed: Expected: {}, Got: {}", expectedStatusCode, outputStatusCode)
         }
-        
+
         expectedBody := `{"endpoints": "/v1/"}`
         outputBodyBytes, _ := ioutil.ReadAll(response.Body)
         outputBody := string(outputBodyBytes)
-        
+
         if outputBody != expectedBody {
             t.Error("Test Failed: Expected: {}, Got: {}", expectedBody, outputBody)
         }
     })
-    
+
     t.Run("with no credentials, returns 401 unauthorized", func(t *testing.T) {
         request, _ := http.NewRequest(http.MethodGet, "/", nil)
         responseWriter := httptest.NewRecorder()
-        
+
         DefaultHandler(responseWriter, request)
         response := responseWriter.Result()
 
         expectedStatusCode := 401
         outputStatusCode := response.StatusCode
-        
+
         if outputStatusCode != expectedStatusCode {
             t.Error("Test Failed: Expected: {}, Got: {}", expectedStatusCode, outputStatusCode)
         }
     })
-    
+
     t.Run("with invalid credentials, returns 403 forbidden", func(t *testing.T) {
         request, _ := http.NewRequest(http.MethodGet, "/", nil)
         request.SetBasicAuth("foo", "bar")
         responseWriter := httptest.NewRecorder()
-        
+
         DefaultHandler(responseWriter, request)
         response := responseWriter.Result()
 
         expectedStatusCode := 403
         outputStatusCode := response.StatusCode
-        
+
         if outputStatusCode != expectedStatusCode {
             t.Error("Test Failed: Expected: {}, Got: {}", expectedStatusCode, outputStatusCode)
         }
