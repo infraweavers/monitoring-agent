67e5e2fdc4f865a52470cfef5f127e5bbb68d544
infraweavers <infraweavers@codeweavers.net>
2021-02-11 18:44:49 +0000

    accurately record the name of each test case

--- internal/httpserver/defaulthandler_test.go
+++ internal/httpserver/defaulthandler_test.go
@@ -1,45 +1,46 @@
 package httpserver
 
 import (
-    "net/http"
-    "net/http/httptest"
-    "testing"
+	"net/http"
+	"net/http/httptest"
+	"testing"
 )
 
 type credential struct {
-    username string
-    password string
+	username string
+	password string
 }
 
 func tester(t *testing.T, cred credential) int {
-    request, _ := http.NewRequest(http.MethodGet, "/", nil)
-    responseWriter := httptest.NewRecorder()
+	request, _ := http.NewRequest(http.MethodGet, "/", nil)
+	responseWriter := httptest.NewRecorder()
 
-    if cred.username != "" && cred.password != "" {
-        request.SetBasicAuth("cred.username", "cred.password")
-    }
+	if cred.username != "" && cred.password != "" {
+		request.SetBasicAuth("cred.username", "cred.password")
+	}
 
-    DefaultHandler(responseWriter, request)
-    return responseWriter.Result().StatusCode
+	DefaultHandler(responseWriter, request)
+	return responseWriter.Result().StatusCode
 }
 
 func TestDefaultHandler(t *testing.T) {
-    var testCases = []struct {
-        testCredential credential
-        expectedResult int
-    }{
-        {credential{"", ""}, http.StatusUnauthorized},
-        {credential{"foo", "bah"}, http.StatusForbidden},
-    }
+	var testCases = []struct {
+		name           string
+		testCredential credential
+		expectedResult int
+	}{
+		{"with no credentials, returns 401 unauthorized", credential{"", ""}, http.StatusUnauthorized},
+		{"with incorrect credentials, returns 401 forbidden", credential{"foo", "bah"}, http.StatusForbidden},
+	}
 
-    for _, testCase := range testCases {
-        t.Run("with no credentials, returns 401 unauthorized", func(t *testing.T) {
+	for _, testCase := range testCases {
+		t.Run(testCase.name, func(t *testing.T) {
 
-            outputStatusCode := tester(t, testCase.testCredential)
+			outputStatusCode := tester(t, testCase.testCredential)
 
-            if outputStatusCode != testCase.expectedResult {
-                t.Error("Test Failed: Expected: {}, Got: {}", testCase.expectedResult, outputStatusCode)
-            }
-        })
-    }
+			if outputStatusCode != testCase.expectedResult {
+				t.Error("Test Failed: Expected: {}, Got: {}", testCase.expectedResult, outputStatusCode)
+			}
+		})
+	}
 }
