f603752282d761aebdd909503bb6803be0447f2b
A Codeweavers Infrastructure Bod <36475663+infraweavers@users.noreply.github.com>
2021-02-12 12:12:40 +0000

    Setting all the timeouts I can find

--- internal/httpserver/httpserver.go
+++ internal/httpserver/httpserver.go
@@ -8,6 +8,8 @@ import (
 	"time"
 )
 
+const requestTimeout = 30 * time.Second
+
 // Launch instantiates a multiplexer and uses it to configure and launch an HTTP server
 func Launch(configurationDirectory string) {
 
@@ -28,8 +30,10 @@ func Launch(configurationDirectory string) {
 		TLSConfig: &tls.Config{
 			Certificates: []tls.Certificate{tlsCert},
 		},
-		WriteTimeout: 30 * time.Second,
-		ReadTimeout:  30 * time.Second,
+		WriteTimeout:      requestTimeout,
+		ReadTimeout:       requestTimeout,
+		ReadHeaderTimeout: requestTimeout,
+		IdleTimeout:       requestTimeout,
 	}
 	log.Println("Launching web server: https://0.0.0.0:9000")
 	log.Fatal(server.ListenAndServeTLS("", ""))
