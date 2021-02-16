dce0b07265100c278ca66b7192b8d5206086dfc5
infraweavers <infraweavers@codeweavers.net>
2021-02-16 12:45:53 +0000

    Make runnable as service + compatible with Windows service API

--- cmd/main.go
+++ cmd/main.go
@@ -6,9 +6,29 @@ import (
 	"mama/internal/web"
 	"os"
 	"path/filepath"
+
+	"github.com/kardianos/service"
 )
 
+var logger service.Logger
+
+type program struct{}
+
+func (p *program) Start(s service.Service) error {
+	// Start should not block. Do the actual work async.
+	go p.run()
+	return nil
+}
+func (p *program) run() {
+	web.Launch()
+}
+func (p *program) Stop(s service.Service) error {
+	// Stop should not block. Return with a few seconds.
+	return nil
+}
+
 func main() {
+
 	executable, error := os.Executable()
 	if error != nil {
 		panic(error)
@@ -18,5 +38,23 @@ func main() {
 
 	logwrapper.Initialise()
 
-	web.Launch()
+	svcConfig := &service.Config{
+		Name:        "GoServiceExampleSimple",
+		DisplayName: "Go Service Example",
+		Description: "This is an example Go service.",
+	}
+
+	prg := &program{}
+	mamasrv, error := service.New(prg, svcConfig)
+	if error != nil {
+		logwrapper.Log.Fatalf(error.Error())
+	}
+	logger, error = mamasrv.Logger(nil)
+	if error != nil {
+		logwrapper.Log.Fatalf(error.Error())
+	}
+	error = mamasrv.Run()
+	if error != nil {
+		logwrapper.Log.Fatalf(error.Error())
+	}
 }
