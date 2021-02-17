5d860d76cefeb261918f4917b4960e22f1f7dad4
A Codeweavers Infrastructure Bod <36475663+infraweavers@users.noreply.github.com>
2021-02-17 13:31:29 +0000

    Adding handler for outputting memory for monitoring the monitoring

--- internal/web/apiv1_info.go
+++ internal/web/apiv1_info.go
@@ -1,12 +1,25 @@
 package web
 
 import (
+	"encoding/json"
 	"net/http"
+	"runtime"
 )
+
+// InfoResult internal information about the go runtine
+type InfoResult struct {
+	Memory runtime.MemStats
+}
 
 // APIV1InfoGetHandler creates a http response for the API /info path
 func APIV1InfoGetHandler(reponseWriter http.ResponseWriter, request *http.Request) {
-	reponseWriter.Header().Set("Content-Type", "application/json")
+	reponseWriter.Header().Set("Content-Type", "application/json; charset=UTF-8")
 	reponseWriter.WriteHeader(http.StatusOK)
-	reponseWriter.Write([]byte(`{"info": ["Not yet implemented. Will return agent info (version, memory consumption, cpu utilisation etc.)"]}`))
+
+	response := InfoResult{}
+	runtime.ReadMemStats(&response.Memory)
+
+	resultJSON, _ := json.Marshal(response)
+
+	reponseWriter.Write(resultJSON)
 }
