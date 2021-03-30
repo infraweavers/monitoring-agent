51b4ba4c910df1fd117524db9f5ad6e11baba3a3
infraweavers <infraweavers@codeweavers.net>
2021-03-30 12:31:46 +0100

    add new endpoint to home output

--- internal/web/apiv1_home.go
+++ internal/web/apiv1_home.go
@@ -8,5 +8,5 @@ import (
 func APIV1HomeGetHandler(reponseWriter http.ResponseWriter, request *http.Request) {
 	reponseWriter.Header().Set("Content-Type", "application/json")
 	reponseWriter.WriteHeader(http.StatusOK)
-	reponseWriter.Write([]byte(`{"endpoints": ["runscript", "info"]}`))
+	reponseWriter.Write([]byte(`{"endpoints": ["runscript", "runscriptstdin", "info"]}`))
 }
