eba1e03de24c847793c45e65985a21e3b90f7032
A Codeweavers Infrastructure Bod <36475663+infraweavers@users.noreply.github.com>
2021-04-07 17:12:50 +0100

    Adding tests around the networking whitelisting, however this absolutely is broken with IPv6 due to the ":" used in the IPv6 addresses. So I think there _has_ to be a better way to get access to the remote IP...

--- internal/web/helpers_test.go
+++ internal/web/helpers_test.go
@@ -2,6 +2,7 @@ package web
 
 import (
 	"mama/internal/configuration"
+	"net"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -39,4 +40,71 @@ HkxuqHSvipJo/unNKgDS+JGDB0+Q5d8nOeoJ0NGOnKBNsNdvAj8FWf7fhaPV7mzRJ1ooLvYpI0yUsD7l
 		response := verifySignature(testBody, testSignature)
 		assert.False(t, response)
 	})
+}
+
+func TestVerifyRemoteHost(t *testing.T) {
+
+	TestSetup()
+	defer TestTeardown()
+
+	t.Run("Empty AllowedAddresses should block all IPs", func(t *testing.T) {
+		configuration.TestingInitialise()
+		configuration.Settings.AllowedAddresses = []*net.IPNet{}
+
+		assert.False(t, verifyRemoteHost("127.0.0.1:5431"))
+	})
+
+	t.Run("Loopback should be permitted when granted", func(t *testing.T) {
+		configuration.TestingInitialise()
+		configuration.Settings.AllowedAddresses = []*net.IPNet{
+			{IP: net.ParseIP("127.0.0.0"), Mask: net.IPMask(net.ParseIP("255.0.0.0").To4())},
+		}
+		assert.True(t, verifyRemoteHost("127.0.0.1:5431"))
+	})
+
+	t.Run("0.0.0.0/0 should grant all", func(t *testing.T) {
+		configuration.TestingInitialise()
+		configuration.Settings.AllowedAddresses = []*net.IPNet{
+			{IP: net.ParseIP("0.0.0.0"), Mask: net.IPMask(net.ParseIP("0.0.0.0").To4())},
+		}
+		assert.True(t, verifyRemoteHost("54.74.60.0:6428"))
+	})
+
+	t.Run("An IP outside of the range should be denied", func(t *testing.T) {
+		configuration.TestingInitialise()
+		configuration.Settings.AllowedAddresses = []*net.IPNet{
+			{IP: net.ParseIP("192.168.54.0"), Mask: net.IPMask(net.ParseIP("255.255.255.0").To4())},
+		}
+		assert.False(t, verifyRemoteHost("8.8.8.8:9945"))
+	})
+
+	t.Run("Only IPs within any of the masks should be granted ", func(t *testing.T) {
+		configuration.TestingInitialise()
+		configuration.Settings.AllowedAddresses = []*net.IPNet{
+			{IP: net.ParseIP("192.168.54.0"), Mask: net.IPMask(net.ParseIP("255.255.255.0").To4())},
+			{IP: net.ParseIP("192.168.51.0"), Mask: net.IPMask(net.ParseIP("255.255.255.0").To4())},
+			{IP: net.ParseIP("8.8.0.0"), Mask: net.IPMask(net.ParseIP("255.255.0.0").To4())},
+		}
+		assert.True(t, verifyRemoteHost("8.8.8.8:9945"))
+		assert.True(t, verifyRemoteHost("192.168.51.54:1"))
+		assert.True(t, verifyRemoteHost("192.168.54.204:1"))
+
+		assert.False(t, verifyRemoteHost("127.0.0.1:5421"))
+		assert.False(t, verifyRemoteHost("10.1.1.1:5874"))
+		assert.False(t, verifyRemoteHost("[::1]:5421"))
+		//assert.False(t, verifyRemoteHost("[ec9b:434a:0623:2620:9fa3:5432:ee23:ea81]:5421"))
+	})
+	/*
+		t.Run("IPv6 should be supported", func(t *testing.T) {
+			configuration.TestingInitialise()
+			configuration.Settings.AllowedAddresses = []*net.IPNet{
+				{IP: net.ParseIP("192.168.54.0"), Mask: net.IPMask(net.ParseIP("255.255.255.0").To4())},
+				{IP: net.ParseIP("ec9b:434a:0623:2620::"), Mask: net.IPMask(net.ParseIP("ffff:ffff:ffff:ffff::").To16())},
+				{IP: net.ParseIP("127.0.0.0"), Mask: net.IPMask(net.ParseIP("255.0.0.0").To4())},
+			}
+
+			assert.False(t, verifyRemoteHost("[::1]:5421"))
+			assert.True(t, verifyRemoteHost("[ec9b:434a:0623:2620:9fa3:5432:ee23:ea81]:5421"))
+		})
+	*/
 }
