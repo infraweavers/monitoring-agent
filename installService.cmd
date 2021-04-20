217b9c2a9c7443f56f5f35963cccd0b457384130
infraweavers <infrastructure@codeweavers.net>
2021-04-20 18:16:19 +0100

    redundant; build the MSI instead or use the instructions on the wiki

--- installService.cmd
+++
@@ -1,4 +0,0 @@
-@echo off
-echo "Installing `Monitoring Agent` Service"
-echo "Running sc create"
-sc create "Monitoring Agent" start= auto error= critical binpath= %~dp0\monitoring-agent.exe
--- uninstallService.cmd
+++
@@ -1,4 +0,0 @@
-@echo off
-echo "Uinstalling `Monitoring Agent` Service"
-echo "Running sc delete"
-sc delete "Monitoring Agent"
