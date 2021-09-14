a9958265a8813441eb81860d7eb772062e2bc5d6
infraweavers <infraweavers@cwserverfarm.local>
2021-09-14 11:02:04 +0100

    make paths testable; with overriding UnmarshalJSON, configuration.Paths gets overwritten with nil values, if the values are not included in the config, which is usually the case.

--- internal/configuration/paths.go
+++ internal/configuration/paths.go
@@ -2,6 +2,7 @@ package configuration
 
 import (
 	"encoding/json"
+	"path/filepath"
 )
 
 // Paths is a struct for storing key paths. The ConfigurationDirectory itself is passed in as a flag
@@ -29,4 +30,24 @@ func (paths *Paths) UnmarshalJSON(b []byte) error {
 	}
 
 	return nil
+}
+
+func InitialisePaths(configurationDirectory string) Paths {
+	return Paths{
+		ConfigurationDirectory: configurationDirectory,
+		CertificatePath:        filepath.FromSlash(configurationDirectory + "/server.crt"),
+		PrivateKeyPath:         filepath.FromSlash(configurationDirectory + "/server.key"),
+	}
+}
+
+func (paths *Paths) mmmmm(p Paths) {
+	paths.ConfigurationDirectory = p.ConfigurationDirectory
+
+	if Settings.Paths.CertificatePath == "" {
+		Settings.Paths.CertificatePath = paths.CertificatePath
+	}
+
+	if Settings.Paths.PrivateKeyPath == "" {
+		Settings.Paths.PrivateKeyPath = paths.PrivateKeyPath
+	}
 }
