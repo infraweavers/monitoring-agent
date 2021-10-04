6d664a7fa8f77e2cef10cd309c9ea086a837666a
Benjamin Davis <BenjaminDavis@sta.cwserverfarm.local>
2021-10-04 10:54:27 +0100

    Updated test case to actually test a configured path that doesn't exist.

--- internal/web/apiv1_runscriptstdin_test.go
+++ internal/web/apiv1_runscriptstdin_test.go
@@ -345,4 +345,36 @@ JkeUlACQaVsrlHmFWg0U0Y5AcnbusFKHNF4bF3kGyixXS3B3/fCZ9T9LMyMbPwZyUJyMGBpfAVXgAQQd
 		assert.Equal(http.StatusBadRequest, output.ResponseStatus, "Response code should be Bad Request")
 		assert.Equal(`{"exitcode":3,"output":"400 Bad Request - Script Arguments Passed But Not Permitted"}`, output.ResponseBody, "Body did not match expected output")
 	})
+
+	t.Run("Tries to run invalid path configured but incorrect, returns HTTP status 200 and error output", func(t *testing.T) {
+		expectedOutput := ""
+		configuration.Settings.Security.ApprovedExecutablesOnly.IsTrue = true
+		configuration.Settings.Security.SignedStdInOnly.IsTrue = true
+
+		osSpecificRunScript := osSpecificRunScriptStdinTestCases[runtime.GOOS].ScriptAsStdInToRun
+
+		osSpecificRunScript.Path = "sh"
+		osSpecificRunScript.Args = []string{"-s"}
+
+		if runtime.GOOS == "windows" {
+			osSpecificRunScript.Path = "sh"
+			osSpecificRunScript.Args = []string{"-s"}
+			expectedOutput = `{"exitcode":3,"output":"An error ocurred executing the command: exec: \"sh\": executable file not found in %PATH%"}`
+		}
+		if runtime.GOOS == "linux" {
+			osSpecificRunScript.Path = `C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe`
+			osSpecificRunScript.Args = []string{"-command", "-"}
+			expectedOutput = ""
+		}
+
+		jsonBody, _ := json.Marshal(osSpecificRunScript)
+		request, _ := http.NewRequest(http.MethodPost, GetTestServerURL(t)+"/v1/runscriptstdin", bytes.NewBuffer(jsonBody))
+
+		output := TestHTTPRequestWithDefaultCredentials(t, request)
+
+		assert := assert.New(t)
+
+		assert.Equal(http.StatusOK, output.ResponseStatus, "Response code should be OK")
+		assert.Equal(expectedOutput, output.ResponseBody)
+	})
 }
